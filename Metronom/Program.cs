e5d3135e5246f869f9d03baf263fd7ffe368169c
MacDeath667 <rom12.93@bk.ru>
2019-01-18 12:13:36 +0300

    init commit

--- Metronom/Program.cs
+++ Metronom/Program.cs
@@ -1,15 +1,40 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
-namespace Metronom
+class TimerExample
 {
-    class Program
+    static void Main()
     {
-        static void Main(string[] args)
+        var autoEvent = new AutoResetEvent(false);          // Create an AutoResetEvent to signal the timeout threshold in the timer callback has been reached.
+        var statusChecker = new StatusChecker(5);
+        var stateTimer = new Timer(statusChecker.CheckStatus, autoEvent, 1500, 1000);
+        autoEvent.WaitOne();         // When autoEvent signals, change the period to every half second.
+        stateTimer.Dispose();
+    }
+}
+
+class StatusChecker
+{
+    private int invokeCount;
+    private int maxCount;
+
+    public StatusChecker(int count)
+    {
+        invokeCount = 0;
+        maxCount = count;
+    }
+
+    public void CheckStatus(Object stateInfo)                       // This method is called by the timer delegate.
+    {
+        AutoResetEvent autoEvent = (AutoResetEvent)stateInfo;
+        Console.WriteLine("{0} Checking status {1,2}.", 
+            DateTime.Now.ToString("h:mm:ss.fff"), (++invokeCount).ToString());
+        Console.Beep();
+        if (invokeCount == maxCount)
         {
+            // Reset the counter and signal the waiting thread.
+            invokeCount = 0;
+            autoEvent.Set();
         }
     }
 }
